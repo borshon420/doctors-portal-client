a912ccbd7281a02807e9abf373ef065d72b14e2d
borshon420 <borshonarafat420@gmail.com>
2021-11-16 02:23:53 +0600

    stripe payment method add

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -7,6 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
 
 const Appointments = ({date}) => {
     const [appointments, setAppointments] = useState([]);
@@ -46,7 +47,9 @@ const Appointments = ({date}) => {
               </TableCell>
               <TableCell align="right">{row.time}</TableCell>
               <TableCell align="right">{row.serviceName}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.payment ? 'Paid' :
+              <Link to={`dashboard/payment/${row._id}`}><button>Pay</button></Link> 
+              }</TableCell>
             </TableRow>
           ))}
         </TableBody>
---
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -0,0 +1,35 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51Jw0q5JGgRtvgVXUsgvlgm1p9etOIgmOWTvPzlhbViNTXg4XLZQ2roaoRrfD2IidzqIye7WqIlGdfSTaS4TtLWcM0094jXyGYa"
+);
+
+const Payment = () => {
+  const { appointmentId } = useParams();
+  const [appointment, setAppointment] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/appointments/${appointmentId}`)
+      .then((res) => res.json())
+      .then((data) => setAppointment(data));
+  }, [appointmentId]);
+  return (
+    <div>
+      <h2>
+        Please pay for: {appointment.patientName} for {appointment.serviceName}
+      </h2>
+      <h2>Pay: ${appointment.price}</h2>
+      {appointment?.price && <Elements stripe={stripePromise}>
+        <CheckoutForm
+        appointment={appointment}
+        />
+      </Elements>}
+    </div>
+  );
+};
+
+export default Payment;
