4a93d81dfcc39971ce1f1270fba75eee36055757
borshon420 <borshonarafat420@gmail.com>
2021-11-17 02:25:57 +0600

    file upload

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import { Button, Input, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const AddDoctor = () => {
-    return (
-        <div>
-            <h3>Add a Doctor</h3>
-        </div>
-    );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = (e) => {
+    if (!image) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("image", image);
+
+    fetch('http://localhost:5000/doctors', {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h3>Add a Doctor</h3>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          sx={{ width: "50%" }}
+          required
+          label="Name"
+          onChange={(e) => setName(e.target.value)}
+          variant="standard"
+        />
+        <br />
+        <TextField
+          sx={{ width: "50%" }}
+          required
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          variant="standard"
+        />
+        <br />
+        <Input
+          accept="image/*"
+          type="file"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <br />
+        <Button variant="contained" type="submit">
+          Add Doctor
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default AddDoctor;
